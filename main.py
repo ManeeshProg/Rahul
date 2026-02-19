4bb1f40ba588a32ed698b42c78ba901c21275c9e
NotManeesh23 <maneeshprog@gmail.com>
2026-02-19 23:20:01 +0530

    first_commit

---
+++ main.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self):
+        self.__mark=0
+    def get_mark(self):
+        return self.__mark
+print("jo")
+
+    
+
+
+student1=Student()
+print(student1.get_mark())
